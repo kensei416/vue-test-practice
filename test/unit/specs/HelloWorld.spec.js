1551a422cbd74d3284d7026aebd12d58c66f67fb
My Name <kensei416@gmail.com>
2017-12-30 10:40:37 +0700

    First VueTest

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import HelloWorld from '@/components/HelloWorld'
 
+function getInstance (Component, propsData) {
+  const Ctor = Vue.extend(Component)
+  const vm = new Ctor({ propsData }).$mount()
+  return vm
+}
+
 describe('HelloWorld.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
-    const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello h1').textContent)
+    const instance = getInstance(HelloWorld, {person: 'john'})
+    expect(instance.$el.querySelector('.hello h1').textContent)
     .to.equal('Welcome to Your Vue.js App')
   })
 })
